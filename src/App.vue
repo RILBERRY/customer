53e0d3455129dc3bb5b12abeef23ccd58d0fdf37
RILBERRY <67415494+RILBERRY@users.noreply.github.com>
2022-05-27 22:35:40 +0500

    created html css pages
    -created patient, island and address  page to list patient details
    - created form to enter patient information

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
+<!-- HTML -->
 <template>
- 
+<div class="PageTop">
+  <div class="TopCont">
+    <h1 class="heading">Patient Registry</h1>
+    <nav>
+      <a href="/patient">PATIENT DETAILS</a> 
+      <a href="/address">ADDRESS DETAILS</a> 
+      <a href="/island">ISLAND DETAILS</a> 
+    </nav>
+  </div>
+</div>
+<PatientsForm/>
 </template>
 
+<!-- SCRIPT -->
 <script>
+import PatientsForm from './components/Patients/Patients-NewForm'
+// import Islands from './components/Islands-index'
+// import Addresses from './components/Addresses-index'
+
 export default {
-  name: 'App',
- 
- 
+  components: {
+    // Patients,
+    // Islands,
+    PatientsForm
+  },
 }
+
 </script>
 
+<!-- STYLE -->
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,4 +44,46 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+body{
+  background-color:#e1e1e1;
+}
+.PageTop {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 140px;
+  background-color:#4EA8A8;
+ 
+}
+.TopCont{
+  position: relative;
+  top: 50px;
+  max-width: 90%;
+  margin:auto;
+  display: flex;
+  justify-content:space-between;
+}
+.heading{
+  position: relative;
+  font-size:30px;
+  color:#fff;
+}
+nav{
+  width:500px;
+  display: flex;
+  justify-content:space-evenly;
+  align-items:end;
+
+}
+nav a{
+  text-decoration: none;
+  color: #fff;
+  font-weight: bold;
+  padding: 10px;
+  border-radius: 10px;
+}
+nav a:hover{
+  background-color:#2c3e50;
+}
+
 </style>
